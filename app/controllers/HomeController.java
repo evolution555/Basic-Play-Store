fdc037794e86f5d23d0d8a90f76882e6809b149b
Evan <evan_conlon_@hotmail.com>
2017-06-23 01:06:10 +0100

    Broken Login

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,45 @@
 package controllers;
 
+import controllers.*;
+import controllers.routes;
+import play.api.Environment;
 import play.mvc.*;
 
+import sun.rmi.runtime.Log;
 import views.html.*;
+import play.data.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import javax.inject.Inject;
+
+import models.users.*;
+import models.*;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+    private FormFactory formFactory;
+    private Environment env;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    @Inject
+    public HomeController(FormFactory f, Environment e) {
+        this.formFactory = f;
+        this.env = e;
+    }
+
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Project is ready"));
+    }
+
+    public Result login() {
+        Form<Login> loginForm = formFactory.form(Login.class);
+        return ok(login.render(loginForm));
     }
 
 }
